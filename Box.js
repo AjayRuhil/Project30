bfff1787b5e2cb6a8635bb9951b749794a8eec6d
AjayRuhil <71335040+AjayRuhil@users.noreply.github.com>
2020-11-05 13:29:44 +0530

    Add files via upload

---
+++ Box.js
@@ -0,0 +1,25 @@
+class Block{
+  constructor(x, y, width, height){
+
+    var options = {
+      'restitution':0.3,
+      'friction':0.1,
+      'density':0.5
+   
+  }
+
+    this.body = Bodies.rectangle(x,y,width,height,options);
+    this.width = width;
+    this.height = height;
+
+    World.add(world, this.body);
+  }
+
+  display(){
+    var pos = this.body.position;
+
+    rectMode(CENTER);
+    rect(pos.x,pos.y,this.width,this.height);
+  }
+
+};
